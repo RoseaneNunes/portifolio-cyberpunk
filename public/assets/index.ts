912af26225ea22868228c0b73830ef0d856355ba
RoseneNunes <roseanesje2014@gmail.com>
2026-08-31 20:18:56 -0300

    chore(assets): adicionar icones e logos do site
    Inclui icones de stacks, redes sociais e logos dos projetos em public/assets.

---
+++ public/assets/index.ts
@@ -0,0 +1,23 @@
+export const linkedinIcon = "/assets/icons/linkedin.svg";
+export const githubIcon = "/assets/icons/github.svg";
+
+export const stackJavaIcon = "/assets/icons/java.svg";
+export const stackReactIcon = "/assets/icons/react.svg";
+export const stackNextJsIcon = "/assets/icons/nextdotjs.svg";
+export const stackVueIcon = "/assets/icons/vuedotjs.svg";
+export const stackViteIcon = "/assets/icons/vite.svg";
+export const stackJavaScriptIcon = "/assets/icons/javascript.svg";
+export const stackTypeScriptIcon = "/assets/icons/typescript.svg";
+export const stackSanityIcon = "/assets/icons/sanity.svg";
+export const stackGitIcon = "/assets/icons/git.svg";
+export const stackHtmlIcon = "/assets/icons/html5.svg";
+export const stackCssIcon = "/assets/icons/css3.svg";
+export const stackFigmaIcon = "/assets/icons/figma.svg";
+
+export const portfolioIrisComunicaLogo = "/assets/logos/iriscomunica.png";
+export const portfolioElevenJr = "/assets/logos/elevenJr.png";
+export const portfolioLíderi = "/assets/logos/Líderi.svg";
+
+export const logoCommercialHub = "/assets/logos/CommercialHub.svg";
+export const logoEA = "/assets/logos/EA.svg";
+export const logoPringles = "/assets/logos/pringles.svg";
